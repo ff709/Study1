6403517d4c3d78d5ecc3bfc9f001b050a9979790
ff <3163617493@qq.com>
2025-05-08 13:50:45 +0800

    测试提交

---
+++ fatigue-monitoring-desktop-pet-main/DesktopPet-lxh_code/ChatBot/deepseek/config/get_yaml_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import yaml
+import os
+
+def get_yaml_config(file_name):
+    """读取yaml配置文件"""
+    config_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = get_yaml_config('device.yaml')
+#print(config)
